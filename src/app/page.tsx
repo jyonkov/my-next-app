5a4f4fba93164991829c7fb4ef12b968052d0a58
jyonkov <yonel@yahoo.com>
2025-08-17 06:09:26 +0000

    Refactor layout and styling: streamline header and footer structure, remove unused font imports, and clean up Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import MyButton from './MyButton';
 import NameForm from './NameForm';
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,9 +8,6 @@ export default function Home() {
       <h1>Welcome to Next.js!</h1>
       <MyButton label="Click Me" />
       <NameForm />
-      <Link href="/about" className="text-blue-600 hover:underline">
-        About This App
-      </Link>
       <Image
         src="/next.svg"
         alt="Next.js Logo"
